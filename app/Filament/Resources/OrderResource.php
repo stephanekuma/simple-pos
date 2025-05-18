755477ce07ba634e411c4d6968cf359abe84749f
Stéphane Kuma <kumastephane@gmail.com>
2025-05-18 20:22:22 +0000

    feat: add Order resource with create, edit and list pages

---
+++ app/Filament/Resources/OrderResource.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\OrderResource\Pages;
+use App\Filament\Resources\OrderResource\RelationManagers;
+use App\Models\Order;
+use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class OrderResource extends Resource
+{
+    protected static ?string $model = Order::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-cart';
+
+    protected static ?int $navigationSort = 1;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        $currencySymbol = config('settings.currency_symbol');
+
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')->label('ID')->sortable(),
+                Tables\Columns\TextColumn::make('customer.firstname')
+                    ->label('Customer Name')
+                    ->numeric()
+                    ->sortable()
+                    ->searchable()
+                    ->formatStateUsing(fn($record) => $record->customer->first_name . ' ' . $record->customer->last_name),
+                Tables\Columns\TextColumn::make('total_price')
+                    ->formatStateUsing(fn($record) => $currencySymbol . ' ' . $record->total_price)->sortable(),
+                // Tables\Columns\TextColumn::make('discount_price')
+                //     ->numeric()
+                //     ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: false),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->defaultSort('id', 'desc')
+            ->filters([
+                Filter::make('created_at')
+                    ->form([
+                        DatePicker::make('start_date')
+                            ->label('From Date'),
+                        DatePicker::make('end_date')
+                            ->label('To Date'),
+                    ])
+                    ->query(function ($query, array $data) {
+                        return $query
+                            ->when($data['start_date'] ?? null, fn($query, $date) => $query->whereDate('created_at', '>=', $date))
+                            ->when($data['end_date'] ?? null, fn($query, $date) => $query->whereDate('created_at', '<=', $date));
+                    })
+                    ->indicateUsing(function (array $data) {
+                        $indicators = [];
+
+                        if (!empty($data['start_date'])) {
+                            $indicators[] = 'From: ' . $data['start_date'];
+                        }
+
+                        if (!empty($data['end_date'])) {
+                            $indicators[] = 'To: ' . $data['end_date'];
+                        }
+
+                        return $indicators;
+                    }),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListOrders::route('/'),
+            // 'create' => Pages\CreateOrder::route('/create'),
+            'edit' => Pages\EditOrder::route('/{record}/edit'),
+        ];
+    }
+}
